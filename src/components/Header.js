f9c103790d1a2d42667fe25f6780e26070e75bb9
Tom Burns <Tomulvlup@gmail.com>
2023-09-22 22:36:35 +0100

    Added Three.js galaxy to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Galaxy from './Galaxy';
 
 const Header = () => {
   return (
-    <header className="text-4xl bg-blue-500 p-4">
-      My Portfolio
+    <header className="relative">
+      <Galaxy />
+      <div className="absolute top-0 left-0 text-4xl text-white p-4">
+        My Portfolio
+      </div>
     </header>
   );
 };
