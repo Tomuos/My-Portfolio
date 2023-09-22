9046a5cdab4a6fe88e448b41330d90c4aaed7db8
Tom Burns <Tomulvlup@gmail.com>
2023-09-22 11:10:20 +0100

    Building the skeleton

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="text-4xl bg-blue-500 p-4">
+      My Portfolio
+    </header>
+  );
+};
+
+export default Header;
