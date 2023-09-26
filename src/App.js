851b4469169dd1d42c837814c1feee73904ca0fb
Tom Burns <Tomulvlup@gmail.com>
2023-09-26 21:13:10 +0100

    few fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+// App.js
+
 import React from 'react';
+import StickyMenu from './components/StickyMenu';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import StickyMenu from './components/StickyMenu'; // Don't forget to import your new StickyMenu component
-
+import Parallax from './components/Parallax';
 
 
 function App() {
   return (
     <div className="App">
-      <StickyMenu /> {/* Add your new StickyMenu component */}
+    <StickyMenu />
       <Header />
       <AboutMe />
+      <Parallax />
       <Projects />
       <Contact />
       <Footer />
